377daa451d39b690187dbc9d933fa303d4df1471
AndreasChristensen89 <brogaard89@gmail.com>
2021-07-26 10:54:10 +0000

    Add generate_ship() vert+horizon options, change board setup, working on valdate data for new setup

--- run.py
+++ run.py
@@ -7,19 +7,30 @@ def main_menu():
         print("1: Single Player Game")
         print("2: Multiplayer Game")
         print("3: Rules\n")
-        user_choice = int(input("Enter choice: "))
+        user_choice = input("Enter choice: ")
 
-        if(user_choice == 1 or user_choice == 2):
+        if(validate_choice(user_choice)):
+            if int(user_choice) == 1:
+                os.system('clear')
+                start_game()
+            elif int(user_choice) == 2:
+                high_scores()
+                os.system('clear')
             break
-        else:
-            print("Value must be 1 or 2, please try again")
 
-    if user_choice == 1:
+
+def validate_choice(choice):
+    try:
+        int(choice)
+        if int(choice) > 3 or int(choice) < 1:
+            raise ValueError("Choice not valid")
+    except ValueError as e:
         os.system('clear')
-        start_game()
-    elif user_choice == 2:
-        high_scores()
+        print(f"Invalid data: {e}, input must be numbers within range")
+        print(input("Press any key to continue"))
         os.system('clear')
+        return False
+    return True
 
 
 def high_scores():
@@ -28,29 +39,65 @@ def high_scores():
 
 def start_game():
     board = []
-    for x in range(0, 5):
-        board.append(["O"] * 5)
+    board_rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    board.append([' ', '1', '2', '3', '4', '5', '6', '7', '8'])
+    for x in range(0, 8):
+        board.append([board_rows[x], "o", "o", "o", "o", "o", "o", "o", "o"])
 
     def print_board(board):
         for i in board:
             print(" ".join(i))
 
-    print("Good Luck!")
     print_board(board)
     print("\n")
 
     ask_for_choices(board)
 
 
+def generate_computer_ship():
+    # dict_board = {
+    #     "A": [1, 2, 3, 4, 5, 6, 7, 8],
+    #     "B": [1, 2, 3, 4, 5, 6, 7, 8],
+    #     "C": [1, 2, 3, 4, 5, 6, 7, 8],
+    #     "D": [1, 2, 3, 4, 5, 6, 7, 8],
+    #     "E": [1, 2, 3, 4, 5, 6, 7, 8],
+    #     "F": [1, 2, 3, 4, 5, 6, 7, 8],
+    #     "G": [1, 2, 3, 4, 5, 6, 7, 8],
+    #     "H": [1, 2, 3, 4, 5, 6, 7, 8]
+    # }
+
+    vertical_horizontal = randint(1, 2)
+
+    ship_lenght = randint(1, 3)
+
+    if vertical_horizontal == 1:
+        row_max_letter = chr(ord('I') - ship_lenght)
+        vert_row = chr(randint(ord('A'), ord(row_max_letter)))
+        column = randint(1, 8)
+
+        vert_rows = []
+        for i in range(ship_lenght):
+            vert_rows.append(chr(ord(vert_row) + i))
+
+        return [vert_rows, column]
+    elif vertical_horizontal == 2:
+        hor_row = chr(randint(ord('A'), ord('H')))
+        hor_col = randint(1, (8-ship_lenght))
+
+        hor_cols = []
+        for i in range(ship_lenght):
+            hor_cols.append(hor_col + i)
+
+        return [hor_row, hor_cols]
+
+
 def ask_for_choices(board):
-    attemps = 5
-    comp_row = randint(0, len(board) - 1)
-    comp_col = randint(0, len(board[0]) - 1)
+    attemps = 10
+    computer_choice = generate_computer_ship()
+    print(computer_choice)
 
     while True:
         print(f"Attempts left: {attemps}")
-        # print(comp_row)
-        # print(comp_col)
 
         guess_row = input("Guess a row: ")
         guess_col = input("Guess a column: ")
@@ -61,9 +108,10 @@ def ask_for_choices(board):
                 os.system('clear')
                 print("Game Over")
                 break
-            elif int(guess_row) == comp_row and int(guess_col) == comp_col:
+            elif (guess_row in computer_choice[0] and
+                  int(guess_col) in computer_choice[1]):
                 os.system('clear')
-                print("Congratulations! You sunk the ship!")
+                print("Nice! You hit the ship!")
                 break
             else:
                 if board[int(guess_row)][int(guess_col)] == "X":
@@ -80,12 +128,13 @@ def ask_for_choices(board):
 def validate_data(guess_row, guess_col, board):
     try:
         int(guess_col)
-        int(guess_row)
-        if int(guess_row) > 4 or int(guess_col) > 4:
+        if int(guess_col) > 9:
             raise ValueError("out of bounds")
+        elif not isinstance(guess_row, int):
+            raise ValueError("invalid input")
     except ValueError as e:
         os.system('clear')
-        print(f"Invalid data: {e}, input must be numbers within range")
+        print(f"Invalid data: {e}, row must be letter, column must be number")
         print(input("Press any key to continue"))
         os.system('clear')
         add_board(board)
@@ -101,7 +150,6 @@ def update_board(board, guess_row, guess_col):
 
 
 def add_board(board):
-    print("board")
     for i in board:
         print(" ".join(i))
     print("\n")
@@ -116,3 +164,4 @@ def main():
 
 print("Welcome to Battleships")
 main()
+# generate_computer_ship()
