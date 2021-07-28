7886705525228d95174ae9de9c71d5cb6fc7c652
AndreasChristensen89 <brogaard89@gmail.com>
2021-07-28 10:35:02 +0000

    Successfully connect project to personal Google Sheet and successfully pulled data

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 add lucidchart for code logic
 
 Remember to add \n to input messages for deployment
+
+When installing pip I got the follow error-message:
+WARNING: You are using pip version 21.1.3; however, version 21.2.1 is available.
+You should consider upgrading via the '/home/gitpod/.pyenv/versions/3.8.11/bin/python3 -m pip install --upgrade pip' command.
+
+This has not been installed yet
--- run.py
+++ run.py
@@ -1,5 +1,21 @@
 from random import randint
 import os
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Battleship Highscores')
+dif_one = SHEET.worksheet('Difficulty 1')
+data = dif_one.get_all_values()
+print(data)
 
 
 def main_menu():
@@ -277,5 +293,5 @@ def main():
     main_menu()
 
 
-print("Welcome to Battleships")
-main()
+# print("Welcome to Battleships")
+# main()
