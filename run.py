581ccf1ea7602f375f08cdd0b397e9d150cb08b2
AndreasChristensen89 <brogaard89@gmail.com>
2021-07-29 09:05:52 +0000

    Try print(n*100) for clear commands to test on Heroku

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from random import randint
-import os
+# import os
 from operator import itemgetter
 import gspread
 from google.oauth2.service_account import Credentials
@@ -25,8 +25,7 @@ def main_menu():
     """
     """
     while True:
-        # os.system('clear')
-        print("\n"*100)
+        print("\n"*24)
         print("1: Start game")
         print("2: Rules")
         print("3: High scores")
@@ -35,29 +34,25 @@ def main_menu():
 
         if(validate_choice(user_choice, 4)):
             if user_choice == "1":
-                # os.system('clear')
-                print("\n"*100)
+                print("\n"*24)
                 set_difficulty()
                 break
             elif user_choice == "2":
                 rules()
-                # os.system('clear')
-                print("\n"*100)
+                print("\n"*24)
                 break
             elif user_choice == "3":
                 show_high_scores()
                 break
             elif user_choice == "4":
-                # os.system('clear')
-                print("\n"*100)
+                print("\n"*24)
                 print("Ciao")
                 break
 
 
 def set_difficulty():
     while True:
-        # os.system('clear')
-        print("\n"*100)
+        print("\n"*24)
         print("Set the difficulty\n")
         print("1: One ship")
         print("2: Two ships")
@@ -77,10 +72,10 @@ def validate_choice(choice, num_of_choices):
         if int(choice) > num_of_choices or int(choice) < 1:
             raise ValueError("Choice not valid")
     except ValueError as e:
-        os.system('clear')
+        print("\n"*24)
         print(f"Invalid data: {e}, input must be numbers within range")
         print(input("Press any key to continue\n"))
-        os.system('clear')
+        print("\n"*24)
         return False
     return True
 
@@ -88,7 +83,7 @@ def validate_choice(choice, num_of_choices):
 def rules():
     """
     """
-    os.system('clear')
+    print("\n"*24)
     print("The rules of Battleship: \n")
     print("You have limited attempts to sink the ships.")
     print("You decide how many ships there will be,")
@@ -108,7 +103,7 @@ def rules():
 def show_high_scores():
     """
     """
-    os.system('clear')
+    print("\n"*24)
     while True:
         print("Select a list to view\n")
         print("1: One ship")
@@ -122,7 +117,7 @@ def show_high_scores():
                 main_menu()
                 break
             else:
-                os.system('clear')
+                print("\n"*24)
                 dif_two = SHEET.worksheet(f'Difficulty {list_choice}')
                 data = dif_two.get_all_values()
                 data_sorted = sorted(data, key=itemgetter(1))
@@ -133,6 +128,7 @@ def show_high_scores():
                 print(input("\nPress 'Enter', or any key, to return\n"))
                 main_menu()
                 break
+            break
 
 
 def start_game(difficulty_choice):
@@ -143,7 +139,7 @@ def start_game(difficulty_choice):
     Ships are generated, difficulty level passed to know number of ships.
     Ask_for_choices() is called, board and ships generated are passed.
     """
-    os.system('clear')
+    print("\n"*24)
     board = []
     board_rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
     board.append([' ', '1', '2', '3', '4', '5', '6', '7'])
@@ -172,12 +168,12 @@ def ask_for_choices(board, ship, difficulty_choice):
 
     while True:
         if attempts == 0:
-            os.system('clear')
+            print("\n"*24)
             print("Game Over\n")
             end_of_game()
             break
         elif sorted(hit_count) == sorted(ship):
-            os.system('clear')
+            print("\n"*24)
             win_game(attempts, difficulty_choice)
             break
         print(f"Attempts left: {attempts}")
@@ -190,17 +186,17 @@ def ask_for_choices(board, ship, difficulty_choice):
             coordinate = board[char_one][char_two]
 
             if coordinate == "X" or coordinate == "O":
-                os.system('clear')
+                print("\n"*24)
                 add_board(board)
                 print("This point has already been guessed")
             elif guess.upper() in ship:
-                os.system('clear')
+                print("\n"*24)
                 hit_count.append(guess.upper())
                 update_board(board, guess, "O")
                 print(hit_count)
                 print(ship)
             else:
-                os.system('clear')
+                print("\n"*24)
                 attempts -= 1
                 update_board(board, guess, "X")
 
@@ -273,10 +269,10 @@ def validate_data(guess, board):
         elif not test_str or not test_len == 2:
             raise ValueError("invalid input")
     except ValueError as e:
-        os.system('clear')
+        print("\n"*24)
         print(f"Error: {e}, must be letter and number within range")
         print(input("Press any key to continue"))
-        os.system('clear')
+        print("\n"*24)
         add_board(board)
         return False
     return True
@@ -328,11 +324,11 @@ def win_game(attempts, difficulty_choice):
 
         if(validate_choice(choice, 3)):
             if choice == "1":
-                os.system('clear')
+                print("\n"*24)
                 register_high_score(attempts, difficulty_choice)
                 break
             elif choice == "2":
-                os.system('clear')
+                print("\n"*24)
                 main_menu()
                 break
             elif choice == "3":
@@ -348,7 +344,7 @@ def register_high_score(attempts, difficulty_choice):
     """
     while True:
         name = input("Enter your name: (Max 10 letters) \n")
-        os.system('clear')
+        print("\n"*24)
         if len(name) <= 10 and len(name) > 0:
             update_high_score(name, attempts, difficulty_choice)
             break
@@ -377,7 +373,7 @@ def end_of_game():
 
         if validate_choice(choice, 2):
             if choice == "1":
-                os.system('clear')
+                print("\n"*24)
                 main_menu()
             elif choice == "2":
                 break
